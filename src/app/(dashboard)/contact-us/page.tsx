319a36f11c67fae07ce36a376789e1d5584665bc
kevinhuang <z5689999@ad.unsw.edu.au>
2025-10-06 11:14:23 +1100

    sfsdf

--- src/app/(dashboard)/contact-us/page.tsx
+++ src/app/(dashboard)/contact-us/page.tsx
@@ -1,25 +1,19 @@
-'use client';
+"use client"
 
-import {
-  PageCard,
-  PageCardContent,
-  PageCardFooter,
-  PageCardHeader,
-  PageCardTitle,
-} from '@/components/ui/pagecard';
-import { TextArea } from '@/components/ui/textarea';
-import { Button } from '@/components/ui/button';
-import { Divider } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { PageCard, PageCardContent, PageCardFooter, PageCardHeader, PageCardTitle } from "@/components/ui/pagecard";
+import { TextArea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Divider } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ContactUs() {
-  const router = useRouter();
-  const [supportContent, setSupportContent] = useState<String | null>(null);
-  const [loadingSubmission, setLoadingSubmission] = useState(false);
-  const [error, setError] = useState<String | null>();
+    const router = useRouter();
+    const [ supportContent, setSupportContent ] = useState<String | null>(null);
+    const [ loadingSubmission, setLoadingSubmission ] = useState(false);
+    const [ error, setError ] = useState<String | null>();
 
-  useEffect(() => {
+    useEffect(() => {
     const originalAlert = window.alert;
 
     window.alert = function customAlert(msg) {
@@ -36,40 +30,39 @@ export default function ContactUs() {
     };
   }, []);
 
-  const handleSubmit = async () => {
-    try {
-      setLoadingSubmission(true);
-      const res = await fetch(`/api/ticket`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ content: supportContent }),
-      });
+    const handleSubmit = async () => {
+        try {
+            setLoadingSubmission(true);
+            const res = await fetch(`/api/ticket`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ content: supportContent })
+            });
 
-      const jsonRes = await res.json();
+            const jsonRes = await res.json();
 
-      if (jsonRes.error) {
-        console.error('Error occurred: ', jsonRes.error);
-        setError(jsonRes.error);
-      }
+            if (jsonRes.error) {
+                console.error("Error occurred: ", jsonRes.error);
+                setError(jsonRes.error);
+            }
 
-      const id = jsonRes.id;
-      router.push(`/contact-us/ticket/${id}`);
-    } catch (err) {
-      console.error('Error occurred: ', err);
-      setError('An error occurred');
+            const id = jsonRes.id;
+            router.push(`/contact-us/ticket/${id}`);
+        } catch (err) {
+            console.error("Error occurred: ", err);
+            setError("An error occurred");
+        }
     }
-  };
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <PageCard>
-          <PageCardHeader>
-            <PageCardTitle>Contact Us</PageCardTitle>
-          </PageCardHeader>
-          <Divider variant="middle" flexItem />
-          <div className="inline">
+    return (
+      <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+          <PageCard>
+            <PageCardHeader>
+              <PageCardTitle>Contact Us</PageCardTitle>
+            </PageCardHeader>
+            <Divider variant="middle" flexItem />
             <PageCardContent className="pt-6 items-start justify-center gap-5">
               {loadingSubmission ? (
                 <p>Loading submission...</p>
@@ -82,24 +75,23 @@ export default function ContactUs() {
                     onChange={(e) => setSupportContent(e.target.value)}
                     className="h-30"
                   />
-                  <div className="flex flex-col items-start gap-2 w-full">
-                    <h1 className="font-bold">Preview: </h1>
-                    <div
-                      className="w-xl"
-                      dangerouslySetInnerHTML={{ __html: supportContent ?? '' }}
-                    />
-                    <br />
-                  </div>
                 </>
               )}
+              <div className="flex flex-col items-start gap-2 w-full">
+                <h1 className="font-bold">Preview: </h1>
+                <div
+                  className="w-xl"
+                  dangerouslySetInnerHTML={{ __html: supportContent ?? '' }}
+                />
+                <br />
+              </div>
             </PageCardContent>
-          </div>
-          <PageCardFooter className="flex flex-col">
-            {error && <p className="text-red-500">{error}</p>}
-            <Button onClick={handleSubmit}>Submit</Button>
-          </PageCardFooter>
-        </PageCard>
-      </main>
-    </div>
-  );
+            <PageCardFooter className="flex flex-col">
+              {error && <p className="text-red-500">{error}</p>}
+              <Button onClick={handleSubmit}>Submit</Button>
+            </PageCardFooter>
+          </PageCard>
+        </main>
+      </div>
+    );
 }
